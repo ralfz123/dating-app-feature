cf00ae1a0ca24aa5f3fb693e0fa0e3ece31f89f5
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-02-27 13:24:14 +0100

    partials aangemaakt

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('static'));
-app.set('view engine', 'ejs')
-app.get('/:userQuery', function(req, res) {
-    res.render('index', {
-        data: {
-            userQuery: req.params.userQuery,
-            searchResults: ['book1', 'book2', 'book3'],
-            loggedIn: true,
-            username: 'Rodneydeb_'
-        }
-    });
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('head');
 });
 
 app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/static/404.html')
 });
+
 
 app.listen(3000, () => console.log('App is listening on port 3000!'));
