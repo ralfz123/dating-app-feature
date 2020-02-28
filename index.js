e6a1b4fd55e3ff6cf4b730833c0834941927ac99
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-02-28 16:40:31 +0100

    linter geconfigureerd

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ app.get('/*', function(req, res) {
 
 
 
-app.listen(3000, () => console.log('App is listening on port 3000!'));
+app.listen(3000, () => console.log('App is listening on port', port));
