37c11b4bb81fd4c48bda965526e6982c6fe6d40d
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-03 12:07:22 +0100

    linter toegevoegd

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
 app.use(express.static('static'));
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
 // Routes
