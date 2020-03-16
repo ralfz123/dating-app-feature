be3ad4aa0279f0a4613c9d6e213fcb4db03d9878
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-16 13:50:14 +0100

    Added posthandler for registration form

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongo = require('mongodb');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Database
 
@@ -22,7 +29,6 @@ mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function(err, clien
     db = client.db(process.env.DB_NAME);
 });
 
-
 // Routes
 // Root
 app.get('/', function(req, res) {
@@ -37,8 +43,28 @@ app.post('/log-in', function(req, res) {
     res.render('loading-login');
 });
 app.post('/registrating', function(req, res) {
-    res.render('loading-registration');
+    let voornaam = req.body.voornaam;
+    let achternaam = req.body.achternaam;
+    let geboorteDatum = req.body.geboortedatum;
+    let email = req.body.email;
+    let wachtwoord = req.body.wachtwoord;
+
+    let data = {
+        'voornaam': voornaam,
+        'achternaam': achternaam,
+        'geboortedatum': geboorteDatum,
+        'email': email,
+        'wachtwoord': wachtwoord,
+    };
+    db.collection('users').insertOne(data, function(err, collection) {
+        if (err) throw err;
+        console.log('Record inserted Successfully');
+
+    });
+
+    return res.render('loading-registration');
 });
+
 // Loading succes
 app.get('/succes', function(req, res) {
     res.render('readytostart');
@@ -49,5 +75,5 @@ app.get('/*', function(req, res) {
 });
 
 
-
+// Welke poort het live staat
 app.listen(3000, () => console.log('App is listening on port', port));
