f0fb519ecd630fbf2477c69b059a22cabe15828d
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-17 11:30:42 +0100

    mongoose toepast ipv mongodb.connect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Variabelen
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,13 +8,71 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const session = require('express-session');
+const connectionString = 'mongodb+srv://rodney:Welkom01@users-zpefv.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
+// App settings
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/registration', function(req, res) {
+    res.render('registration');
+});
+app.post('/log-in', function(req, res) {
+    res.render('loading-login');
+});
+app.post('/registrating', function(req, res) {
+    let voornaam = req.body.voornaam;
+    let achternaam = req.body.achternaam;
+    let geboorteDatum = req.body.geboortedatum;
+    let email = req.body.email;
+    let wachtwoord = req.body.wachtwoord;
+
+    let data = {
+        'voornaam': voornaam,
+        'achternaam': achternaam,
+        'geboortedatum': geboorteDatum,
+        'email': email,
+        'wachtwoord': wachtwoord,
+    };
+    db.collection('users').insertOne(data, function(err, collection) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Record inserted Successfully');
+            return res.render('loading-registration');
+        }
+    });
+
+
+});
+app.get('/succes', function(req, res) {
+    res.render('readytostart');
+});
+app.get('/*', function(req, res) {
+    res.render('404');
+});
+
+// Database
+require('dotenv').config();
+let db = null;
+let url = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_URL + process.env.DB_END;
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, client) {
+    if (err) {
+        console.log('Database is niet connected');
+    } else if (client) {
+        console.log('Connectie met database is live');
+    }
+});
 
 // Mongoose schema voor gebruiker
 let UserSchema = new mongoose.Schema({
@@ -45,70 +104,6 @@ let User = mongoose.model('User', UserSchema);
 module.exports = User;
 
 
-
-// Database
-
-require('dotenv').config();
-let db = null;
-
-let url = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_URL + process.env.DB_END;
-
-mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
-    if (err) {
-        console.log('Database is niet connected');
-    } else if (client) {
-        console.log('Connectie met database is live');
-    }
-    db = client.db(process.env.DB_NAME);
-});
-
-// Routes
-// Root
-app.get('/', function(req, res) {
-    res.render('index');
-});
-// Registration
-app.get('/registration', function(req, res) {
-    res.render('registration');
-});
-// Loading
-app.post('/log-in', function(req, res) {
-    res.render('loading-login');
-});
-app.post('/registrating', function(req, res) {
-    let voornaam = req.body.voornaam;
-    let achternaam = req.body.achternaam;
-    let geboorteDatum = req.body.geboortedatum;
-    let email = req.body.email;
-    let wachtwoord = req.body.wachtwoord;
-
-    let data = {
-        'voornaam': voornaam,
-        'achternaam': achternaam,
-        'geboortedatum': geboorteDatum,
-        'email': email,
-        'wachtwoord': wachtwoord,
-    };
-    db.collection('users').insertOne(data, function(err, collection) {
-        if (err) {
-            throw err;
-        } else {
-            console.log('Record inserted Successfully');
-            return res.render('loading-registration');
-        }
-    });
-
-
-});
-
-// Loading succes
-app.get('/succes', function(req, res) {
-    res.render('readytostart');
-});
-// 404
-app.get('/*', function(req, res) {
-    res.render('404');
-});
 
 
 // Welke poort het live staat
